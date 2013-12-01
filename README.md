8342cff482427292b5ee4e9c04d420455217d08a
Hari KT <kthari85@gmail.com>
2013-12-01 10:45:08 +0530

    Fixed the attr helper, added escapeHtml helper. Added branch alias as per @simensen's recommendation

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ This library is installable and autoloadable via Composer with the following
 `require` element in your `composer.json` file:
 
     "require": {
-        "aura/html": "dev-develop-2"
+        "aura/html": "2.*@dev"
     }
     
 Alternatively, download or clone this repository, then require or include its
--- scripts/helper_registry.php
+++ scripts/helper_registry.php
@@ -3,9 +3,10 @@ namespace Aura\Html;
 
 return array(
     'anchor'        => function () { return new Helper\Anchor; },
-    'attr'          => function () { return new Helper\Attribs; },
+    'attr'          => function () { return new Helper\EscapeAttr; },
     'base'          => function () { return new Helper\Base; },
     'form'          => function () { return new Helper\Form; },
+    'escapeHtml'    => function () { return new Helper\EscapeHtml; },
     'input'         => function () {
         $helper_locator = new HelperLocator(new HelperFactory(
             new Escaper,
