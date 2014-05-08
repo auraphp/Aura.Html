db49ac2765c26bb316ad8a1230a237ea8a034f5d
Paul M. Jones <pmjones88@gmail.com>
2014-05-08 12:58:13 -0500

    Input helper does not need to extend AbstractHelper; pass HelperLocator in constructor

--- src/Helper/Input.php
+++ src/Helper/Input.php
@@ -19,7 +19,7 @@ use Aura\Html\HelperLocator;
  * @package Aura.Html
  * 
  */
-class Input extends AbstractHelper
+class Input
 {
     /**
      * 
@@ -32,14 +32,12 @@ class Input extends AbstractHelper
     
     /**
      * 
-     * Sets the locator object.
+     * Constructor.
      * 
-     * @param HelperLocator $helper_locator
-     * 
-     * @return null
+     * @param HelperLocator $helper_locator A locator for input helpers.
      * 
      */
-    public function setHelperLocator(HelperLocator $helper_locator)
+    public function __construct(HelperLocator $helper_locator)
     {
         $this->helper_locator = $helper_locator;
     }
